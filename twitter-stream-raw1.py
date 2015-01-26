5f84907a072eb4dd4b58dce2634f24ede6181eb8
Benjamin Mako Hill <mako@atdot.cc>
2015-01-26 13:09:25 -0800

    added a version that gives us raw json from the streaming API

---
+++ twitter-stream-raw1.py
@@ -0,0 +1,30 @@
+import json
+import tweepy
+from twitter_authentication import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth, parser=tweepy.parsers.RawParser)
+
+@classmethod                    
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+tweepy.models.Status.first_parse = tweepy.models.Status.parse
+tweepy.models.Status.parse = parse
+
+class StreamListener(tweepy.StreamListener):
+    def on_status(self, tweet):
+        print tweet.json
+
+    def on_error(self, status_code):
+        print 'Error: ' + repr(status_code)
+        return False
+
+l = StreamListener()
+streamer = tweepy.Stream(auth=auth, listener=l)
+
+streamer.sample()
