1b5bb6edc34a0c0f3b4e3b97a7df8aa709ba9c9e
Benjamin Mako Hill <mako@atdot.cc>
2015-05-04 17:15:57 -0700

    fixed bugs and python3 updates for the twitter projects

--- twitter4.py
+++ twitter4.py
@@ -8,8 +8,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # code to write the file
-import codecs
-output_file = codecs.open("MY_DATA.tsv", "w", "utf-8")
+output_file = open("MY_DATA.tsv", "w", encoding="utf-8")
 
 public_tweets = api.search("data science", count=10)
 
