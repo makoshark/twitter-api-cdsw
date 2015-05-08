1e2d406ccc7bfaa9c73ab809367ce5ae2ab6fc79
Benjamin Mako Hill <mako@atdot.cc>
2015-05-07 17:45:56 -0700

    included a cursor example

---
+++ twitter1-cursor.py
@@ -0,0 +1,16 @@
+import encoding_fix
+import tweepy
+from twitter_authentication import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+import time
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+# I found the idea of how to the user the Cursor here:
+#   https://tweepy.readthedocs.org/en/v3.2.0/cursor_tutorial.html
+for page in tweepy.Cursor(api.home_timeline, count=200).pages():
+    for tweet in page:
+        print(tweet.text)
+    time.sleep(1)
