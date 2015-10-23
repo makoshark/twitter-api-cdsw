ba48aab4fca5486068104ad61b2d37db6f7cc86a
Benjamin Mako Hill <mako@atdot.cc>
2015-10-22 19:42:29 -0700

    updated cursor to new version of tweepy docs

--- twitter1-cursor.py
+++ twitter1-cursor.py
@@ -9,7 +9,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # I found the idea of how to the user the Cursor here:
-#   https://tweepy.readthedocs.org/en/v3.2.0/cursor_tutorial.html
+#   https://tweepy.readthedocs.org/en/v3.4.0/cursor_tutorial.html
 for page in tweepy.Cursor(api.home_timeline, count=200).pages():
     for tweet in page:
         print(tweet.text)
